49b3ddf2ac733709813bc1487112dc1be41eef1d
thunder6230 <62564512+thunder6230@users.noreply.github.com>
2022-05-29 18:55:33 +0200

    added 2 more icons

--- src/utilities/fontawsome.ts
+++ src/utilities/fontawsome.ts
@@ -1,9 +1,9 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faCircleCheck, faFeed, faThumbsUp, faComment, faCodeBranch, faComments, faSliders, faUserGroup, faAddressCard, faCloudSun, faImages  } from "@fortawesome/free-solid-svg-icons";
+import { faCircleCheck, faFeed, faThumbsUp, faComment, faCodeBranch, faComments, faSliders, faUserGroup, faAddressCard, faCloudSun, faImages, faFileImage, faMessage  } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 
-library.add(faCircleCheck, faFeed, faThumbsUp,faComment, faCodeBranch, faComments, faSliders, faUserGroup, faAddressCard, faCloudSun, faImages);
+library.add(faCircleCheck, faFeed, faThumbsUp,faComment, faCodeBranch, faComments, faSliders, faUserGroup, faAddressCard, faCloudSun, faImages, faFileImage, faMessage);
 
 export default FontAwesomeIcon;
